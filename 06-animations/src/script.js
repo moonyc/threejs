d6bf90931b65412c9131b6c1ca2c07d1b1de9d17
muni <booscarface@gmail.com>
2022-12-08 11:58:46 +0100

    requestAnimationFrame serves the purpose of calling the function on the next frame

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -30,3 +30,13 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Animations 
+
+const tick = () => 
+{
+    console.log('tick')
+    window.requestAnimationFrame(tick) // we pass the function, we don't call the function
+}
+
+tick()
