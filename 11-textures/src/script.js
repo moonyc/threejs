545a9ccd75e6bb59e3e12c97d778e93dd518f51d
muni <booscarface@gmail.com>
2023-01-17 15:28:08 +0100

    texture loader

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -4,14 +4,22 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 /**
  * Textures
  */
-const image = new Image()
-const texture = new THREE.Texture(image)
-
-image.onload = () => 
-{
-   texture.needsUpdate = true
-}
-image.src = '/textures/door/color.jpg'
+ const textureLoader = new THREE.TextureLoader()
+ const texture = textureLoader.load(
+    '/textures/door/color.jpg',
+    () =>
+    {
+       console.log('load')
+    },
+    () =>
+    {
+       console.log('progress') // progress never works
+    },
+    () => 
+    {
+      console.log('error')
+    }
+    )
 /**
  * Base
  */
