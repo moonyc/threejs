35d6cef509f6357043db811c1a512192d1376f34
muni <booscarface@gmail.com>
2023-04-16 18:52:36 +0200

    debug

---
+++ 44-debug-a-r3f-application/src/Cube.jsx
@@ -0,0 +1,6 @@
+export default function Cube({ scale = 1 }) {
+    return (<mesh position-x={ 2 } scale={ scale }>
+               <boxGeometry />
+               <meshStandardMaterial color="mediumpurple" />
+           </mesh>)
+}
--- 44-debug-a-r3f-application/src/Experience.jsx
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -1,23 +1,48 @@
 import { OrbitControls } from '@react-three/drei'
+import {  button, useControls } from 'leva'
+import { Perf } from 'r3f-perf'
+import Cube from './Cube'
 
 export default function Experience()
 {
+    const { position, color, visible } = useControls('sphere',{
+        position: {
+            value: { x: -2, y: 0 },
+            joystick: 'invertY',
+            step: 0.01
+        },
+        color: '#ff0000',
+        visible: true,
+        myInterval: 
+        {
+            min: 0,
+            max: 10,
+            value: [4,5]
+        },
+        clickMe: button(() => { console.log('ok')}),
+        choice: { options: ['a', 'b', 'c']}
+    })
+    const {scale} = useControls('cube', {
+        scale: {
+            min: 0,
+            max: 5,
+            value: 1,
+            step: 0.01
+        }
+    })
     return <>
-
+         <Perf position="top-left"/>
         <OrbitControls makeDefault />
 
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
+        <mesh position= { [ position.x, position.y, 0 ] } visible={visible}>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color={color} />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <Cube scale={scale}/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
