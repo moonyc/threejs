86e600350b09b4f08ce1ea77f02375e4143b28b6
muni <booscarface@gmail.com>
2023-04-20 10:27:17 +0200

    load hamburger

--- 46-load-models-with-r3f/src/Experience.jsx
+++ 46-load-models-with-r3f/src/Experience.jsx
@@ -1,8 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { Suspense }  from 'react'
+
+import Model from './Model'
+import Placeholder from './Placeholder'
 
 export default function Experience()
 {
+    
     return <>
 
         <Perf position="top-left" />
@@ -12,20 +17,17 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
+       
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
-
+       
+       <Suspense
+            fallback={ <Placeholder position-y={0.5} scale={[2,3,2]}/>}
+       >
+          <Model />
+       </Suspense>
+       
     </>
 }
