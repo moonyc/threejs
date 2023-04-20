01424e685b6ac9e848f81fd7504cb4113e4ef625
muni <booscarface@gmail.com>
2023-04-20 19:05:12 +0200

    performances

--- 47-3d-text-with-r3f/src/Experience.jsx
+++ 47-3d-text-with-r3f/src/Experience.jsx
@@ -1,18 +1,60 @@
-import { OrbitControls } from '@react-three/drei'
+import { Center, Text3D, OrbitControls, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useState } from 'react'
 
 export default function Experience()
 {
+    const [torusGeometry, setTorusGeometry] = useState()
+    const [material, setMaterial] = useState()
+    const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
     return <>
 
-        <Perf position="top-left" />
+    <Perf position="top-left" />
 
-        <OrbitControls makeDefault />
+    <OrbitControls makeDefault />
+    <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]}/>
+    <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+    <Center>
+        <Text3D 
+            material={material}
+            font="./fonts/helvetiker_regular.typeface.json"
+            size={.75}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.02}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}
+            >
+            SASHA
+            
+        </Text3D>
+    </Center>
+       
+        {[...Array(100)].map((value, index) => 
+            <mesh
+               key={index}
+               geometry={torusGeometry}
+               material={material}
+               position={
+                [
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,                ]
+               }
+               scale={ 0.2 + Math.random() * 0.2}
+               rotation={[
+                Math.random() * Math.PI,
+                Math.random() * Math.PI,
+                0
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+               ]}
+            >
+                
+                <meshMatcapMaterial matcap={matcapTexture}/>
+            </mesh>
+        )}
+   
     </>
 }
