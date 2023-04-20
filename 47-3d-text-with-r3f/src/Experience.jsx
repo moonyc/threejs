f5fab40af71941981b3609a2cdfa3e4fa096cdc6
muni <booscarface@gmail.com>
2023-04-20 19:20:12 +0200

    animate donuts

--- 47-3d-text-with-r3f/src/Experience.jsx
+++ 47-3d-text-with-r3f/src/Experience.jsx
@@ -1,19 +1,51 @@
 import { Center, Text3D, OrbitControls, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useState } from 'react'
+import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
+import { useEffect, useRef, useState } from 'react'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
-    const [torusGeometry, setTorusGeometry] = useState()
-    const [material, setMaterial] = useState()
+    // const [torusGeometry, setTorusGeometry] = useState()
+    // const [material, setMaterial] = useState()
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+    // const donutsGroup = useRef()
+    const donuts = useRef([])
+
+    useEffect(() => {
+        matcapTexture.encoding = THREE.sRGBEncoding
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    // useFrame((state, delta) => 
+    // {
+    //      for(const donut of donutsGroup.current.children)
+    //      {
+    //         donut.rotation.y += delta * 0.2
+    //      }
+    // })
+
+    useFrame((state, delta) => 
+    {
+        for(const donut of donuts.current)
+          {
+            donut.rotation.y += delta * 0.2
+          }
+    })
     return <>
 
     <Perf position="top-left" />
-
     <OrbitControls makeDefault />
-    <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]}/>
-    <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+
+    {/* <torusGeometry ref={setTorusGeometry} args={[1, 0.6, 16, 32]}/>
+    <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
+
     <Center>
         <Text3D 
             material={material}
@@ -31,9 +63,13 @@ export default function Experience()
             
         </Text3D>
     </Center>
-       
+
+    {/* <group 
+      ref={donutsGroup}
+      >    */}
         {[...Array(100)].map((value, index) => 
             <mesh
+               ref={(element) => donuts.current[index] = element}
                key={index}
                geometry={torusGeometry}
                material={material}
@@ -55,6 +91,6 @@ export default function Experience()
                 <meshMatcapMaterial matcap={matcapTexture}/>
             </mesh>
         )}
-   
+    {/* </group> */}
     </>
 }
