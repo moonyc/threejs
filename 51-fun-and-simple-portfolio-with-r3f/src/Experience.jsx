af5e823935dd7db7d5751f5617f21dab1c81a782
muni <booscarface@gmail.com>
2023-04-23 17:37:15 +0200

    portfolio

--- 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,15 +1,63 @@
-import { OrbitControls } from '@react-three/drei'
+import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF} from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF(
+        'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+    )
     return <>
-
-        <OrbitControls makeDefault />
+        <color args={[ '#241a1a' ]} attach="background" />
+        <Environment preset="forest" />
         
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
 
+        <PresentationControls
+           global
+           rotation={[ 0.13, 0.1, 0 ]}
+           polar={[ -0.4, 0.2 ]}
+           azimuth={[ -1, 0.75 ]}
+           config={{ mass: 2, tension: 400}}
+           snap={{ mass: 4, tension: 400}}
+        >
+            <Float rotationIntensity={ 0.4 }>
+                <primitive 
+                    object={ computer.scene }
+                    position-y={ - 1.2} 
+                    position-x={ 0.13 }  
+            >
+               <rectAreaLight 
+                 width={ 2.5 }
+                 height={ 1.65 }
+                 intensity={ 65 }
+                 color={'#ff6900'}
+                 rotation={[ - 0.1, Math.PI, 0 ]}
+                 position={[ 0, 0.55, -1.55 ]}
+               />
+               <Html
+                  transform
+                  wrapperClass='htmlScreen'
+                  distanceFactor={ 1.17 }
+                  position={[ 0, 1.56, -1.4 ]}
+                  rotation-x={ - 0.256 }
+               >
+                  <iframe src="https://bruno-simon.com/html/" /> 
+               </Html>
+            </primitive>
+            </Float>
+        </PresentationControls>
+        <ContactShadows 
+           position-y={ - 1.4 } 
+           opacity={0.4}
+           scale={5}
+           blur={2.4}
+        />
+
+        <Text
+           font='/bangers-v20-latin-regular.woff'
+           fontSize={1}
+           position={[2, 0.75, 0.75]}
+           rotation-y={-1.25}
+           maxWidth={2}
+        > 
+        Portfolio</Text>
     </>
 }
