4dc2576585287f6b11b8bfd36647bd727ed5cfff
muni <booscarface@gmail.com>
2023-04-30 11:15:35 +0200

    active class controls css

--- 53-create-a-game-with-r3f/src/Interface.jsx
+++ 53-create-a-game-with-r3f/src/Interface.jsx
@@ -1,22 +1,31 @@
-
+import { useKeyboardControls } from "@react-three/drei"
 
 export default function Interface()
 {
+    // const controls = useKeyboardControls((state) => state)
+    const forward = useKeyboardControls((state) => state.forward)
+    const backward = useKeyboardControls((state) => state.backward)
+    const leftward = useKeyboardControls((state) => state.leftward)
+    const rightward = useKeyboardControls((state) => state.rightward)
+    const jump = useKeyboardControls((state) => state.jump)
+
+    
+
     return <div className="interface">
         <div className="time">0.00</div>
         <div className="restart">Restart</div>
         {/* Controls */}
         <div className="controls">
            <div className="raw">
-               <div className="key"></div>
+               <div className={`key ${ forward ? 'active' : ''}`}></div>
            </div>
            <div className="raw">
-               <div className="key"></div>
-               <div className="key"></div>
-               <div className="key"></div>
+               <div className={`key ${leftward ? 'active' : ''}`}></div>
+               <div className={`key ${ backward ? 'active' : ''}`}></div>
+               <div className={`key ${ rightward ? 'active' : ''}`}></div>
            </div>
            <div className="raw">
-              <div className="key large"></div>
+              <div className={`key large ${ jump ? 'active' : '' }`}></div>
            </div>
         </div>
     </div>
